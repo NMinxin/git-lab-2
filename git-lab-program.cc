214225f9d4fc77f71dd3652769b732cab905fa42
Nora <nma.anderson25@gmail.com>
2025-05-16 16:18:33 -0400

    Adding files

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Nora Anderson
+ *        Date: 5/16/2025
+ * Description: Git Lab 1
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
